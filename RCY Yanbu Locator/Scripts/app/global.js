384352251eccc04b3cf2e4019bc6305a0dbc0342
athar <mohd.athar@hotmail.com>
2020-08-09 19:43:10 +0800

    popups

--- RCY Yanbu Locator/Scripts/app/global.js
+++ RCY Yanbu Locator/Scripts/app/global.js
@@ -16,9 +16,87 @@
 });
 
 var menuVisible = false;
+var cityPnlVisible = false;
+var bankPnlVisible = false;
+var hotelPnlVisible = false;
+var fuelPnlVisible = false;
+var hospitalPnlVisible = false;
+var salahPnlVisible = false;
+var busroutePnlVisible = false;
+var currencyPnlVisible = false;
+var weatherPnlVisible = false;
+var taxiPnlVisible = false;
+var rulesPnlVisible = false;
 
-function hotelClick() {
-    alert('hotel icon clicked');
+function togglePanel(pnl) {
+    resetPanels();
+
+    switch (pnl) {
+        case 'bank':
+            bankPnlVisible = true;
+            $('.bank-popup').addClass('d-block');
+            break;
+        case 'hotel':            
+            hotelPnlVisible = true;
+            $('.hotel-popup').addClass('d-block');
+            break;
+        case 'fuel':
+            fuelPnlVisible = true;
+            $('.fuel-popup').addClass('d-block');
+            break;
+        case 'hospital':
+            hospitalPnlVisible = true;
+            $('.hospital-popup').addClass('d-block');
+            break;
+        case 'salah':
+            salahPnlVisible = true;
+            $('.salah-popup').addClass('d-block');
+            break;
+        case 'busroute':
+            busroutePnlVisible = true;
+            $('.busroute-popup').addClass('d-block');
+            break;
+        case 'currency':
+            currencyPnlVisible = true;
+            $('.currency-popup').addClass('d-block');
+            break;
+        case 'weather':
+            weatherPnlVisible = true;
+            $('.weather-popup').addClass('d-block');
+            break;
+        case 'taxi':
+            taxiPnlVisible = true;
+            $('.taxi-popup').addClass('d-block');
+            break;
+        case 'rules':
+            rulesPnlVisible = true;
+            $('.rules-popup').addClass('d-block');
+            break;
+    }
+}
+
+function resetPanels() {
+    weatherPnlVisible = false;
+    currencyPnlVisible = false;
+    busroutePnlVisible = false;
+    salahPnlVisible = false;
+    cityPnlVisible = false;
+    bankPnlVisible = false;
+    hotelPnlVisible = false;
+    fuelPnlVisible = false;
+    hospitalPnlVisible = false;
+    taxiPnlVisible = false;
+    rulesPnlVisible = false;
+    $('.bank-popup').removeClass('d-block');
+    $('.hotel-popup').removeClass('d-block');
+    $('.fuel-popup').removeClass('d-block');
+    $('.hospital-popup').removeClass('d-block');
+    $('.salah-popup').removeClass('d-block');
+    $('.busroute-popup').removeClass('d-block');
+    $('.currency-popup').removeClass('d-block');
+    $('.weather-popup').removeClass('d-block');
+    $('.taxi-popup').removeClass('d-block');
+    $('.rules-popup').removeClass('d-block');
 }
 
 function toggleMenu() {
@@ -31,5 +109,9 @@ function toggleMenu() {
 }
 
 function toggleLayer(e, layer) {
-    alert('layer toggle');
+   
+}
+
+function closePopup(){
+    resetPanels();
 }
