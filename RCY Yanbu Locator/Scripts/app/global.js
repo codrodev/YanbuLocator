3df9177aa014df05c680c65d2535c939e40e248b
athar <athar@GAMMA>
2020-09-14 11:33:46 +0400

    upd

--- RCY Yanbu Locator/Scripts/app/global.js
+++ RCY Yanbu Locator/Scripts/app/global.js
@@ -45,6 +45,7 @@ var newsPnlVisible = false;
 var emergencyPnlVisible = false;
 var poiPnlVisible = false;
 var galleryPnlVisible = false;
+var infoPnlVisible = false;
 
 function togglePanel(pnl) {
     resetPanels();
@@ -110,6 +111,10 @@ function togglePanel(pnl) {
             galleryPnlVisible = true;
             $('.gallery-popup').addClass('d-block');
             break;
+        case 'info':
+            infoPnlVisible = true;
+            $('.info-popup').addClass('d-block');
+            break;
     }
 }
 
@@ -131,6 +136,8 @@ function resetPanels() {
     emergencyPnlVisible = false;
     poiPnlVisible = false;
     galleryPnlVisible = false;
+    infoPnlVisible = false;
+    $('.info-popup').removeClass('d-block');
     $('.gallery-popup').removeClass('d-block');
     $('.poi-popup').removeClass('d-block');
     $('.emergency-popup').removeClass('d-block');
